2a274cbd8ac6c58573fe08cfdcba93526e420513
andrerzp <dreprasetyaa@gmail.com>
2022-09-30 15:25:12 +0700

    first commit

---
+++ resources/views/DashboardInstrument/script.blade.php
@@ -0,0 +1,13 @@
+<!-- jQuery -->
+<script src="{{asset('templates/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap -->
+<script src="{{asset('templates/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE -->
+<script src="{{asset('templates/dist/js/adminlte.js')}}"></script>
+
+<!-- OPTIONAL SCRIPTS -->
+<script src="{{asset('templates/plugins/chart.js/Chart.min.js')}}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{asset('templates/dist/js/demo.js')}}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{asset('templates/dist/js/pages/dashboard3.js')}}"></script>
---
+++ resources/views/LandingPageInstrument/script.blade.php
@@ -0,0 +1,6 @@
+<!-- jQuery -->
+<script src="{{asset('templates/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('templates/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('templates/dist/js/adminlte.min.js')}}"></script>
